0a90645ac6a12a3b8eb5f5116072e9c96c5d2834
lmpacent <73558668+lmpacent@users.noreply.github.com>
2021-02-17 15:23:21 -0700

    Add files via upload

---
+++ NB_datalog.py
@@ -0,0 +1,42 @@
+
+#Add Phidgets library
+from Phidget22.Phidget import *
+from Phidget22.Devices.TemperatureSensor import *
+from Phidget22.Devices.PressureSensor import *
+#Required for sleep statement
+import time
+
+#Create
+temperatureSensor = TemperatureSensor()
+pressureSensor = PressureSensor()
+
+#Open
+temperatureSensor.openWaitForAttachment(1000)
+pressureSensor.openWaitForAttachment(1000)
+
+#Create data file
+outfile = open("phidgets_data.csv", "w")
+
+#Record data points
+count = 0
+
+#Use your Phidgets
+while (True):
+    #Update user
+    print("Logging data...")
+    
+    #Write data to file in CSV format
+    outfile.write(time.strftime("%Y-%m-%d %H:%M:%S") + "," + 
+                  str(temperatureSensor.getTemperature()) + "," +
+                  str(pressureSensor.getPressure()) + "\n")
+    
+    #Increment count
+    count += 1
+    
+    #If 10 data points have been recorded, close file and exit program
+    if(count == 10):
+        outfile.close()
+        print("Logging complete, exiting program")
+        exit()
+    
+    time.sleep(1.0)
