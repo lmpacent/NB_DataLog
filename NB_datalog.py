ffab6cf6e84b10f9d097a0cbbe3ddf0945e71ad0
lmpacent <73558668+lmpacent@users.noreply.github.com>
2021-02-18 11:43:35 -0700

    Update NB_datalog.py

--- NB_datalog.py
+++ NB_datalog.py
@@ -13,30 +13,15 @@ pressureSensor = PressureSensor()
 temperatureSensor.openWaitForAttachment(1000)
 pressureSensor.openWaitForAttachment(1000)
 
-#Create data file
-outfile = open("data.csv", "w")
-outfile.write("Date,Temperature,Pressure\n")
-
-#Record data points
-count = 0
-
 #Use your Phidgets
 while (True):
     #Update user
     print("Logging data...")
     
     #Write data to file in CSV format
-    outfile.write(time.strftime("%Y-%m-%d %H:%M:%S") + "," + 
+    with open ('var/www/data.csv','a') as datafile:
+        datafile.write(time.strftime("%Y-%m-%d %H:%M:%S") + "," + 
                   str(temperatureSensor.getTemperature()) + "," +
                   str(pressureSensor.getPressure()) + "\n")
-    
-    #Increment count
-    count += 1
-    
-    #If 10 data points have been recorded, close file and exit program
-    if(count == 10):
-        outfile.close()
-        print("Logging complete, exiting program")
-        exit()
     
     time.sleep(1.0)
